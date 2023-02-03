42563ba0c836183d22fcd593d3676806582843cf
Nick <50417170+kanishev@users.noreply.github.com>
2023-02-03 20:46:08 +0600

    validation function

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,38 @@
+interface Validatable {
+  value: string;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate({ value, required, minLength, maxLength, min, max }: Validatable): boolean {
+  let isValid = true;
+
+  if (required) {
+    isValid = isValid && value.trim().length > 0;
+  }
+
+  if (minLength) {
+    isValid = isValid && value.length >= minLength;
+  }
+
+  if (maxLength) {
+    isValid = isValid && validate.length <= maxLength;
+  }
+
+  if (min != null) {
+    isValid = isValid && +value >= min;
+  }
+
+  if (max != null) {
+    isValid = isValid && +value <= max;
+  }
+
+  return isValid;
+}
+
 function Autobind(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
   const adjDescriptor: PropertyDescriptor = {
     get() {
@@ -38,7 +73,26 @@ class ProjectInput {
     const descrption = this.descriptionInputElement.value;
     const peopleCount = this.peopleCountInputElement.value;
 
-    if (title.trim().length === 0 || descrption.trim().length === 0 || peopleCount.trim().length === 0) {
+    const titleConfig = {
+      value: title,
+      required: true,
+      minLength: 5,
+    };
+
+    const descriptionConfig = {
+      value: descrption,
+      required: true,
+      minLength: 10,
+    };
+
+    const peopleCountConfig = {
+      value: peopleCount,
+      required: true,
+      min: 5,
+      max: 10,
+    };
+
+    if (![titleConfig, descriptionConfig, peopleCountConfig].some((el) => validate(el))) {
       alert("Invalid inputs");
       return;
     } else {
@@ -70,4 +124,4 @@ class ProjectInput {
   }
 }
 
-const prjInput = new ProjectInput();
+new ProjectInput();
